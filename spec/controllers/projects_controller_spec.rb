66d8cd0ccd9f7549f25f019cc51c637bca1fe34b
Rob vanBrandenburg <RobvanB@Gmail.com>
2018-01-14 14:28:35 -0500

    7.1.2 Add admin namespace with application controller + fixes for ruby/rails upgrade

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   it "handles a missing project correctly" do
-    get :show, id: "not-here"
+    get :show, params: { id: "not-here" }
 
     expect(response).to redirect_to(projects_path)
 
